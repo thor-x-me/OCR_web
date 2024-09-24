3de6516e45b16de346884c377135fc05c4b61ad4
thorxme <abhishekverm218@gmail.com>
2024-09-24 07:03:43 +0530

    Gemini vision api script

---
+++ script/GeminiVision.py
@@ -0,0 +1,27 @@
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+
+def getResponseFromImage(image, question=None):
+    load_dotenv()
+
+    GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+
+    genai.configure(api_key=GOOGLE_API_KEY)
+
+    # Upload the file and print a confirmation.
+    sample_file = genai.upload_file(path=image,
+                                display_name="Jetpack drawing")
+
+    print(f"Uploaded file '{sample_file.display_name}' as: {sample_file.uri}")
+
+
+    # Choose a Gemini API model.
+    model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest")
+
+    # Prompt the model with text and the previously uploaded image.
+    response = model.generate_content([sample_file, f"{question}"])
+
+    return response.text
+
